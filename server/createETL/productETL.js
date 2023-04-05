4d0be5bef5f8edcc39d3359120125a9459c8fc2e
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-04-05 16:36:47 -0400

    postgreSQL ETL complete

--- server/createETL/productETL.js
+++ server/createETL/productETL.js
@@ -1,23 +1,152 @@
-const path = require('path')
-const db = require('../database')
+const path = require('path');
+const client = require('../database');
 
-async function buildTables() {
-  db.query('DROP TABLE IF EXISTS ;')
-  .then(() => {
-    db.query(`
-    CREATE TABLE IF NOT EXISTS products (
+async function productsEtl() {
+  try {
+    await client.query(`CREATE TABLE IF NOT EXISTS products (
       id INTEGER PRIMARY KEY,
-      name VARCHAR (30) NOT NULL,
-      slogan VARCHAR (256) NOT NULL,
-      description VARCHAR (512) NOT NULL,
-      category VARCHAR (20) NOT NULL,
-      default_price VARCHAR (10) NOT NULL
+      name VARCHAR(255),
+      slogan VARCHAR(255),
+      description TEXT,
+      category VARCHAR(255),
+      default_price NUMERIC
     )`)
-  })
-  .then(() => db.query(`
-  COPY products(id, name, slogan, description, category, default_price)
-  FROM '${path.join(__dirname, '../data/product.csv')}'
-  DELIMITER ','
-  CSV HEADER
-  `))
+    const filePath = path.join(__dirname, '../data/products.csv')
+    await client.query(`
+    COPY products(id, name, slogan, description, category, default_price)
+    FROM '${filePath}'
+    DELIMITER ','
+    CSV HEADER
+    `)
+    console.log('products etl completed successfully')
+  } catch (err) {
+    console.error('Error in products ETL:', err)
+  } finally {
+    client.end()
+  }
 }
+
+async function reviewsEtl() {
+  try {
+    await client.query(`CREATE TABLE IF NOT EXISTS reviews (
+      id serial PRIMARY KEY,
+      product_id INTEGER REFERENCES products(id),
+      rating INTEGER,
+      date BIGINT,
+      summary VARCHAR(255),
+      body TEXT,
+      recommend BOOLEAN,
+      reported BOOLEAN,
+      reviewer_name VARCHAR(255),
+      reviewer_email VARCHAR(255),
+      response TEXT,
+      helpfulness INTEGER
+    );`);
+
+    const filePath = path.join(__dirname, '../data/reviews.csv')
+
+    await client.query(`
+      COPY reviews(id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+      FROM '${filePath}'
+      DELIMITER ','
+      CSV HEADER
+    `);
+
+    console.log('Reviews ETL complete');
+  } catch (err) {
+    console.error('Error in reviews ETL:', err);
+  } finally {
+    client.end();
+  }
+}
+
+async function reviewPhotosEtl() {
+  try {
+    await client.query(`
+    CREATE TABLE IF NOT EXISTS reviewPhotos (
+      id SERIAL PRIMARY KEY,
+      review_id INTEGER REFERENCES reviews(id),
+      url VARCHAR(255)
+      )
+    `)
+    const filePath = path.join(__dirname, '../data/reviews_photos.csv')
+
+    await client.query(`
+    COPY reviewPhotos(id, review_id, url)
+    FROM '${filePath}'
+    DELIMITER ','
+    CSV HEADER
+  `);
+  console.log('Data inserted into reviewPhotos table successfully')
+  } catch (err) {
+    console.error('Error during reviewPhotos ETL:', err)
+  } finally {
+    client.end()
+  }
+}
+
+async function characteristicsEtl() {
+  try {
+    await client.query(`CREATE TABLE IF NOT EXISTS characteristics (
+      id SERIAL PRIMARY KEY,
+      product_id INTEGER REFERENCES products(id),
+      name VARCHAR(255)
+    )`)
+    const filePath = path.join(__dirname, '../data/characteristics.csv')
+      await client.query(`
+      COPY characteristics(id, product_id, name)
+      FROM '${filePath}'
+      DELIMITER ','
+      CSV HEADER
+      `)
+      console.log('Characteristics ETL process successfully completed')
+  } catch (err) {
+    console.error('Error during Characteristics ETL process', err)
+  } finally {
+    client.end()
+  }
+}
+
+async function characteristicReviewsEtl() { //  REFERENCES characteristics(id) - potentially add to characteristic_id, circlee back
+  try {
+    await client.query(`CREATE TABLE IF NOT EXISTS characteristicReviews (
+      id SERIAL PRIMARY KEY,
+      characteristic_id INTEGER REFERENCES characteristics(id),
+      review_id INTEGER REFERENCES reviews(id),
+      value INTEGER
+    )`)
+    const filePath = path.join(__dirname, '../characteristic_reviews.csv')
+
+      await client.query(`
+      COPY characteristicReviews(id, characteristic_id, review_id, value)
+      FROM '${filePath}'
+      DELIMITER ','
+      CSV HEADER
+      `)
+    console.log('characteristicReviews ETL finished successfully!')
+  } catch (err) {
+    console.error('Error in characteristicReviews ETL:', err)
+  } finally {
+    client.end()
+  }
+}
+
+async function generateReviews() {
+  await productsEtl();
+  console.log('products table finished')
+  await reviewsEtl()
+  console.log('reviews table finished')
+  await reviewPhotosEtl()
+  console.log('review photos table finished')
+  await characteristicsEtl()
+  console.log('characteristics table finished')
+  await characteristicReviewsEtl()
+  console.log('characteristic reviews table finished')
+}
+
+client.connect().then(async () => {
+  await generateReviews()
+  .catch(err => console.log(err))
+})
+
+module.exports = generateReviews
